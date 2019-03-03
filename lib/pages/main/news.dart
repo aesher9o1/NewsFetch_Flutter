8b6b0820981ceea99b2c8591edab8577f7dec395
aeser9o1 <aashiskumar986@gmail.com>
2019-03-03 17:44:41 +0530

    Future Builder added

--- lib/pages/main/news.dart
+++ lib/pages/main/news.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class News extends StatelessWidget {
+  var _urlTopNews = "https://hacker-news.firebaseio.com/v0/topstories.json";
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(child: Expanded(
-      child: ListView.builder(
-        itemBuilder: ((BuildContext context, int index) {
-          return null;
-        }),
+    Future<List<String>> _getTopNews() async {
+      List<String> topStoriesID = [];
+      var topNews = await http.get(_urlTopNews);
+      var jsonData = json.decode(topNews.body);
+
+
+      print(jsonData);    
+      for (var stories in jsonData) topStoriesID.add(stories.toString());
+
+      return topStoriesID;
+    }
+
+    return Container(
+        child: Expanded(
+      child: FutureBuilder(
+        future: _getTopNews(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container(
+                child: Center(
+              child: Text("Loading Data"),
+            ));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(""+snapshot.data[index]),
+              );
+            },
+          );
+        },
       ),
     ));
   }
